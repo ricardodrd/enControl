c953d037f5a85a28c6ccf085d665af8ab30502ff
SirArgonZ <40165894+SirArgonZ@users.noreply.github.com>
2018-06-27 15:48:49 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,72 +1,85 @@
-/**FIRST CLASS/
-// d3.select();
-// d3.selectAll();
-//
-//
-// d3.select('h1').style('color','red')
-// .attr('class','heading')
-// .text('Updating h1 text');
-//
-// d3.select('body').append('p').text('First Text Paragraph');
-// d3.select('body').append('p').text('Second Text Paragraph');
-// d3.select('body').append('p').text('Third Text Paragraph');
-//
-// d3.selectAll('p').style('color','blue');
+//API to fetch historical data of Bitcoin Price Index
+const api = 'https://api.coindesk.com/v1/bpi/historical/close.json?start=2017-12-31&end=2018-04-01';
 
-/*SECOND CLASS DATA LOADING*/
-// var dataset = [1,2,3,4,5];
-//
-// d3.select('body')
-//       .selectAll('p')
-//       .data(dataset)
-//       .enter()
-//       .append('p')
-//       .text(function(d) {return d;});
+/**
+ * Loading data from API when DOM Content has been loaded'.
+ */
+document.addEventListener("DOMContentLoaded", function(event) {
+fetch(api)
+    .then(function(response) { return response.json(); })
+    .then(function(data) {
+        var parsedData = parseData(data);
+        drawChart(parsedData);
+    })
+    .catch(function(err) { console.log(err); })
+});
 
-/*THIRD CLASS CREATE BAR CHART*/
-d3.axisTop()
-d3.axisRight()
-d3.axisBottom()
-d3.axisLeft()
-var dataset = [30, 60, 150, 200, 120, 13, 24];
-//var dataset = [1,2,3,4,5];
-var svgWidth = 500, svgHeight = 300, barPadding = 5;
-var barWidth= (svgWidth/dataset.length);
+/**
+ * Parse data into key-value pairs
+ * @param {object} data Object containing historical data of BPI
+ */
+function parseData(data) {
+    var arr = [];
+    for (var i in data.bpi) {
+        arr.push({
+            date: new Date(i), //date
+            value: +data.bpi[i] //convert string to number
+        });
+    }
+    return arr;
+}
+
+/**
+ * Creates a chart using D3
+ * @param {object} data Object containing historical data of BPI
+ */
+function drawChart(data) {
+var svgWidth = 600, svgHeight = 400;
+var margin = { top: 20, right: 20, bottom: 30, left: 50 };
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
 
 var svg = d3.select('svg')
     .attr("width", svgWidth)
     .attr("height", svgHeight);
 
-// var yScale = d3.scaleLinear()
-//         .domain([0, d3.max(dataset)])
-//         .range([0, svgHeight]);
+var g = svg.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-var barChart = svg.selectAll("rect")
-    .data(dataset)
-    .enter()
-    .append("rect")
-    .attr("y", function(d) {
-      return svgHeight - d
-    })
-    .attr("height", function(d) {
-      return d;
-    })
-    .attr("width", barWidth - barPadding)
-    .attr("transform", function(d, i){
-      var translate = [barWidth*i,0];
-      return "translate("+ translate +")";
-    });
+var x = d3.scaleTime()
+    .rangeRound([0, width]);
 
-    var text = svg.selectAll("text")
-        .data(dataset)
-        .enter()
-        .append("text")
-        .text(function(d){
-          return d;
-        })
-        .attr("y", function(d, i){
-          return svgHeight - d - 2;
-        })
-        .attr("x", function(d, i){
-          return barWidth*i;
-        });
+var y = d3.scaleLinear()
+    .rangeRound([height, 0]);
+
+var line = d3.line()
+    .x(function(d) { return x(d.date)})
+    .y(function(d) { return y(d.value)})
+    x.domain(d3.extent(data, function(d) { return d.date }));
+    y.domain(d3.extent(data, function(d) { return d.value }));
+
+g.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(x))
+    .select(".domain")
+    .remove();
+
+g.append("g")
+    .call(d3.axisLeft(y))
+    .append("text")
+    .attr("fill", "#000")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", "0.71em")
+    .attr("text-anchor", "end")
+    .text("Price ($)");
+
+g.append("path")
+    .datum(data)
+    .attr("fill", "none")
+    .attr("stroke", "steelblue")
+    .attr("stroke-linejoin", "round")
+    .attr("stroke-linecap", "round")
+    .attr("stroke-width", 1.5)
+    .attr("d", line);
+}
